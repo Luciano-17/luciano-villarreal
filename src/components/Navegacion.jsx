55f6aadce0d2871c2684f0eee900f12591a4135d
Luciano-17 <lucianovillarreal.cba@gmail.com>
2024-03-10 22:05:03 -0300

    Se agrega el menu mobile y corrige algunos estilos

--- src/components/Navegacion.jsx
+++ src/components/Navegacion.jsx
@@ -17,7 +17,7 @@ const Navegacion = ({width, height}) => {
 
     return (
         <>
-            <div className={`${width <= 998 ? 'navegacion-mobile' : 'navegacion-container'}`}>   
+            <div className={`${width <= 998 ? 'navegacion-mobile' : 'navegacion-container'} ${sidebar ? ' active' : ''}`}>   
                 {width <= 998 ? (
                     <>
                         <button 
@@ -29,17 +29,45 @@ const Navegacion = ({width, height}) => {
                             <span></span>
                         </button>
 
-                        <div className={`sidebar ${sidebar ? 'active' : ''}`}>
+                        <div className={`sidebar${sidebar ? ' active' : ''}`}>
                             <div className='container-inicio'>
-
+                                <a href="#inicio" className='logo'>Villarreal</a>
                             </div>
 
                             <div className='container-nav'>
-
+                                <nav className='navegacion'>
+                                    <a href="#sobreMi" className='nav'>
+                                        <i className="fa-solid fa-address-book"></i>
+                                        Sobre Mi
+                                    </a>
+                                    <a href="#habilidades" className='nav'>
+                                        <i className="fa-solid fa-list"></i>
+                                        Habilidades
+                                    </a>
+                                    <a href="#proyectos" className='nav'>
+                                        <i className="fa-solid fa-briefcase"></i>
+                                        Proyectos
+                                    </a>
+                                    <a href="#contacto" className='nav'>
+                                        <i className="fa-solid fa-envelope"></i>
+                                        Contactame
+                                    </a>
+                                </nav>
                             </div>
 
                             <div className='container-buttons'>
+                                <a href={cv} download="CV - Luciano Villarreal" className='cv-mobile'>
+                                    <i className="fa-solid fa-file-arrow-down icon-tooltip"></i>
+                                </a>
 
+                                <button 
+                                    type="button" 
+                                    className='dark-mode-btn-mobile'
+                                    onClick={() => modoOscuro()}
+                                >
+                                    <i className='fa-solid fa-cloud-moon icon-tooltip luna' style={{display: darkMode ? 'none' : 'block'}}></i>
+                                    <i className='fa-solid fa-sun icon-tooltip sol' style={{display: darkMode ? 'block' : 'none'}}></i>
+                                </button>
                             </div>
                         </div>
                     </>
